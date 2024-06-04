81cebd792c77e24909674a9b6649fdcb4e18ea5f
Elena Khaletska <yeliena.khaletska@pipedrive.com>
2024-06-04 13:10:18 +0300

    Improve themes with card colors and background colors

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -7,14 +7,21 @@
 
 import UIKit
 
+struct Theme {
+    let backgroundColor: UIColor
+    let cardBackColor: UIColor
+    var emojis: Array<String>
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet private weak var flipCountLabel: UILabel!
     @IBOutlet private weak var scoreLabel: UILabel!
     @IBOutlet private var cardButtons: [UIButton]!
 
     private var game: Concentration!
-    private var emojiChoices: Array<String>!
+    private var theme: Theme!
     private var emoji = [Int : String]()
 
     override func viewDidLoad() {
@@ -24,7 +31,6 @@ class ViewController: UIViewController {
 
     @IBAction private func newGameButtonPressed(_ sender: UIButton) {
         startNewGame()
-        updateUI()
     }
 
     @IBAction private func touchCard(_ sender: UIButton) {
@@ -36,7 +42,17 @@ class ViewController: UIViewController {
 
     private func startNewGame() {
         self.game = Concentration(numberOfPairsOfCards: (self.cardButtons.count + 1) / 2)
-        self.emojiChoices = Self.themes.randomElement() ?? []
+        self.theme = Self.themes.randomElement() ?? nil
+        applyTheme()
+        updateUI()
+    }
+
+    private func applyTheme() {
+        self.flipCountLabel.textColor = self.theme.cardBackColor
+        self.scoreLabel.textColor = self.theme.cardBackColor
+        self.newGameButton.configuration?.baseForegroundColor = self.theme.cardBackColor
+        self.newGameButton.configuration?.baseBackgroundColor = self.theme.cardBackColor
+        self.view.backgroundColor = self.theme.backgroundColor
     }
 
     private func updateUI() {
@@ -49,7 +65,7 @@ class ViewController: UIViewController {
                 button.backgroundColor = UIColor.white
             } else {
                 button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? UIColor.clear : UIColor.systemOrange
+                button.backgroundColor = card.isMatched ? UIColor.clear : self.theme.cardBackColor
             }
         }
         self.flipCountLabel.text = "Flips: \(self.game.flipCount)"
@@ -57,9 +73,9 @@ class ViewController: UIViewController {
     }
 
     private func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, self.emojiChoices.count > 0 {
-            let randIndex = Int.random(in: 0..<self.emojiChoices.count)
-            emoji[card.identifier] = self.emojiChoices.remove(at: randIndex)
+        if emoji[card.identifier] == nil, self.theme.emojis.count > 0 {
+            let randIndex = Int.random(in: 0..<self.theme.emojis.count)
+            emoji[card.identifier] = self.theme.emojis.remove(at: randIndex)
         }
 
         return emoji[card.identifier] ?? "?"
@@ -69,17 +85,17 @@ class ViewController: UIViewController {
 
 extension ViewController {
 
-    private static var themes = [
-        ["🧑‍🎓" , "🧑‍🏫", "🧑‍⚖️", "🧑‍🌾", "🧑‍🍳", "🧑‍🔧", "🧑‍🔬", "🧑‍💻", "🧑‍🚀", "🧑‍🚒", "🧑‍✈️", "👮", "🕵️", "💂", "🥷", "👷"], // professions
-        ["🍞", "🥐", "🥖", "🥨", "🥯", "🥞", "🧇", "🍩", "🍪"], // pastry
-        ["🐳", "🦭", "🐬", "🐟", "🐠", "🐡", "🦈", "🐙", "🪼"], // ocean
-        ["🐥", "🕊️", "🦆", "🦅", "🦉", "🦜", "🦚", "🦩", "🐓"], // birds
-        ["🦸", "🦹", "🧙", "🧚", "🧛", "🧜", "🧞", "🧝‍♀️", "🧟"], // creatures
-        ["🍔", "🍟", "🍕", "🌭", "🌮", "🌯", "🍿", "🍣", "🍜"], // street food
-        ["❤️", "🩷", "🧡", "💛", "💚", "💙", "🩵", "💜", "🤎"], // colors
-        ["🍅", "🍆", "🥔", "🥕", "🌽", "🫑", "🥒", "🥦", "🍄‍🟫"], // veggies
-        ["🍇", "🍉", "🥝", "🍌", "🍓", "🍍", "🥥", "🍒", "🍑"], // fruites
-        ["🦁", "🐵", "🦊", "🐯", "🐱", "🐶", "🐮", "🐷", "🐭"], // animals
+    private static var themes: Array<Theme> = [
+        Theme(backgroundColor: UIColor.darkGray, cardBackColor: UIColor.systemYellow, emojis: ["🧑‍🎓" , "🧑‍🏫", "🧑‍⚖️", "🧑‍🌾", "🧑‍🍳", "🧑‍🔧", "🧑‍🔬", "🧑‍💻", "🧑‍🚀", "🧑‍🚒", "🧑‍✈️", "👮", "🕵️", "💂", "🥷", "👷"]),
+        Theme(backgroundColor: UIColor.systemBrown, cardBackColor: UIColor.brown, emojis: ["🍞", "🥐", "🥖", "🥨", "🥯", "🥞", "🧇", "🍩", "🍪"]), // pastry
+        Theme(backgroundColor: UIColor.systemBlue, cardBackColor: UIColor.systemTeal, emojis: ["🐳", "🦭", "🐬", "🐟", "🐠", "🐡", "🦈", "🐙", "🪼"]), // ocean
+        Theme(backgroundColor: UIColor.systemIndigo, cardBackColor: UIColor.systemPink, emojis: ["🦸", "🦹", "🧙", "🧚", "🧛", "🧜", "🧞", "🧝‍♀️", "🧟"]), // creatures
+        Theme(backgroundColor: UIColor.systemGray, cardBackColor: UIColor.systemRed, emojis: ["🍔", "🍟", "🍕", "🌭", "🌮", "🌯", "🍿", "🍣", "🍜"]), // street food
+        Theme(backgroundColor: UIColor.darkGray, cardBackColor: UIColor.gray, emojis: ["❤️", "🩷", "🧡", "💛", "💚", "💙", "🩵", "💜", "🤎"]), // colors
+        Theme(backgroundColor: UIColor.systemGray, cardBackColor: UIColor.systemGreen, emojis: ["🐥", "🕊️", "🦆", "🦅", "🦉", "🦜", "🦚", "🦩", "🐓"]), // birds
+        Theme(backgroundColor: UIColor.systemGray, cardBackColor: UIColor.systemGreen, emojis: ["🍅", "🍆", "🥔", "🥕", "🌽", "🫑", "🥒", "🥦", "🍄‍🟫"]), // veggies
+        Theme(backgroundColor: UIColor.systemGray, cardBackColor: UIColor.systemGreen, emojis: ["🍇", "🍉", "🥝", "🍌", "🍓", "🍍", "🥥", "🍒", "🍑"]), // fruites
+        Theme(backgroundColor: UIColor.systemGray, cardBackColor: UIColor.systemGreen, emojis: ["🦁", "🐵", "🦊", "🐯", "🐱", "🐶", "🐮", "🐷", "🐭"]), // animals
     ]
 
 }
